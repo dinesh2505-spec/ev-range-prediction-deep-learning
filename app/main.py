3f3158ffbc078af5ea714822ccf5eda35980a9db
Dinesh Reddy <dineshreddymale25@gmail.com>
2026-08-04 12:26:07 +0200

    Connected HTML frontend with ANN prediction API

--- app/main.py
+++ app/main.py
@@ -1,9 +1,13 @@
 # Import Libraries
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 
 from app.schemas import EVInput
 
 from src.predictor import predict_range
+
+from fastapi import Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 
 # Create FastAPI instance
 app = FastAPI(
@@ -12,28 +16,54 @@ app = FastAPI(
     version="1.0"
 )
 
+templates = Jinja2Templates(directory="templates")
+
 # Create the home end point
-@app.get("/")
-def home():
-    return {
-        "message": "EV Range Prediction API is running"
-    }
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
 
-@app.post("/predict")
-def predict(ev: EVInput):
-    """
-    Predict EV driving range.
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html"
+    )
 
-    Parameters
-    ----------
-    ev : EVInput
-        Input data for prediction.
+@app.post("/predict", response_class=HTMLResponse)
+def predict(
 
-    Returns
-    -------
-    dict
-        Predicted range in km.
-    """
+    request: Request,
+
+    battery_capacity_kWh: float = Form(...),
+    efficiency_wh_per_km: float = Form(...),
+    torque_nm: float = Form(...),
+    acceleration_0_100_s: float = Form(...),
+    top_speed_kmh: float = Form(...)
+
+):
+
+    predicted_range = predict_range(
+
+        battery_capacity_kWh,
+        efficiency_wh_per_km,
+        torque_nm,
+        acceleration_0_100_s,
+        top_speed_kmh
+
+    )
+
+
+    return templates.TemplateResponse(
+
+        request=request,
+
+        name="index.html",
+
+        context={
+
+            "prediction": round(predicted_range,2)
+
+        }
+
+    )
 
     # Extract input values from the Pydantic model
     battery_capacity_kWh = ev.battery_capacity_kWh
